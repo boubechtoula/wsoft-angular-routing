05ca1aec088407ca8d080f1bfd42c5582332885b
boubechtoula <dw_boubechtoula@esi.dz>
2019-10-28 16:29:11 +0100

    utilisation de ParamMap pour récuper les paramètres de l'url en temps réel

--- src/app/departement-detail/departement-detail.component.ts
+++ src/app/departement-detail/departement-detail.component.ts
@@ -1,20 +1,35 @@
 import { Component, OnInit } from '@angular/core';
-import {ActivatedRoute} from '@angular/router';
+import {ActivatedRoute, Router, ParamMap} from '@angular/router';
 
 @Component({
   selector: 'app-departement-detail',
   template: `
      <h3>You selected departement with id = {{departementId}}</h3>
+     <a  (click)="goSuivant()">Suivant</a>
+     <br>
+     <a (click)="goPrecedent()">précédent</a>
   `,
   styleUrls: ['./departement-detail.component.css']
 })
 export class DepartementDetailComponent implements OnInit {
   public departementId ; 
-  constructor( private root  : ActivatedRoute) { }
+  constructor( private root  : ActivatedRoute, private router : Router) { }
 
   ngOnInit() {
-    let id = parseInt(this.root.snapshot.paramMap.get('id')); 
-  this.departementId = id;
+   // let id = parseInt(this.root.snapshot.paramMap.get('id')); 
+    //this.departementId = id;
+    this.root.paramMap.subscribe((params : ParamMap)=>{
+      let id = parseInt(params.get('id'));
+      this.departementId = id;
+    })
+  }
+  goSuivant(){
+  let suivantId = this.departementId +1;
+  this.router.navigate(['/departement', suivantId]);
+  }
+  goPrecedent(){
+  let  precedentId = this.departementId-1;
+  this.router.navigate(['/departement', precedentId]);
   }
 
 }
